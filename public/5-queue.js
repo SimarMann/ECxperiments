64c121996ec1b99c1a053ceaac79ade2c0ed36f1
SSM022 <simarjit.s.mann@protonmail.com>
2019-12-13 23:16:37 -0500

    a simple queue; that removes from the front and adds to the back of an array

---
+++ public/5-queue.js
@@ -0,0 +1,18 @@
+
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  var removedFromArray = arr.shift();
+  return removedFromArray;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log('Before: ' + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log('After:'  + JSON.stringify(testArr));
+
+
