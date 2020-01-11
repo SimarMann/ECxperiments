3d09c08ee90035680cc7820e440fde0305a32e85
SSM0022 <simarjit.s.mann@protonmail.com>
2020-01-10 23:49:28 -0500

    var vs let scope; var is global or local inside function, let is global or local inside function, block, statement or expression

--- public/19-es6.js
+++ public/19-es6.js
@@ -7,3 +7,33 @@ var a = 1;
 var a = 2; // Overwrites variable
 let b = 1;
 let b = 2; // SyntaxError: Identifier 'b' has already been declared
+
+/*
+Var vs. Let Scope;
+The Var Keyword: declares a variable globally or locally if declared inside a function.
+The Let keyword: behaves the same but inside a block, statement, or expression, 
+its scope is limited to that block, statement, or expression.
+*/
+
+function checkScope() {
+  "use strict";
+  var i = "function scope";
+  console.log("Function scope i is: ", i);
+  if (true) {
+    i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+function checkScope() {
+  "use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
