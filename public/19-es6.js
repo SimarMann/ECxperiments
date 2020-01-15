9b48b0a8370655a0a42b83dbcdd94173de02a08c
SSM0022 <simarjit.s.mann@protonmail.com>
2020-01-14 23:48:43 -0500

    prevent data mutation with Object.freeze; const still allows data in objects to be mutated, object.freeze locks each value

--- public/19-es6.js
+++ public/19-es6.js
@@ -68,3 +68,24 @@ function editInPlace() {
   // Like all arrays the elements in s are mutable
 }
 editInPlace(); // const s = [2, 5, 7]
+
+/*
+Prevent Object Mutation:
+Const functions dont prevent mutation of objects
+Javascript provides Object.freeze to prevent data mutation
+Once the object is frozen, you can no longer add, update, or delete properties from it
+*/
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  Object.freeze(MATH_CONSTANTS);
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
