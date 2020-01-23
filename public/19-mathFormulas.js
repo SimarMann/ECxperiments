f215cd50b60e1844a3cb77795e3f0090266aef0c
SSM0022 <simarjit.s.mann@protonmail.com>
2020-01-22 21:51:43 -0500

    math formulas; two formulas to find the sum of a set of consecutive numbers

---
+++ public/19-mathFormulas.js
@@ -0,0 +1,8 @@
+/* Math formulas in code */
+
+// Two different formulas for finding the sum of a set of consecutive numbers.
+const gaussFormula = n => (n / 2) * (1 + n);
+console.log(gaussFormula(100)); // 5050
+
+const triangluarNumberSequence = n => (n * (n + 1)) / 2;
+console.log(triangluarNumberSequence(100)); //5050
