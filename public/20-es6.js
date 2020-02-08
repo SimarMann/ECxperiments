08fc31c3e3b1c7405fa7ae2836dfe1ca64ea7596
SSM0022 <simarjit.s.mann@protonmail.com>
2020-02-07 23:18:03 -0500

    Destructuring Assignment V; destructuring assignment with the rest parameter is similar to slice(), allows you to collect remaining elements into new array

--- public/20-es6.js
+++ public/20-es6.js
@@ -229,3 +229,20 @@ console.log(a, b); // 1, 2
 
 const [a, b, , , c] = [1, 2, 3, 4, 5, 6]; // access value at any index using commas
 console.log(a, b, c); // 1, 2, 5
+
+// Destructuring Assignment with the Rest Parameter to Reassign Array Elements:
+
+// Example 1
+const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+console.log(a, b); // 1, 2
+console.log(arr); // [3, 4, 5, 7]
+
+// Example 2
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+  const [a, b, ...arr] = list;
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
