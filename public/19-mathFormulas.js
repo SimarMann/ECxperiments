1c1984d83100160a3148869165062fed12448e39
SimarMann <47508510+SimarMann@users.noreply.github.com>
2023-08-10 20:34:22 -0600

    Add leastCommonMultiple function to 19-mathFormulas.js
    The function outputs the Least Common Multiple for a range of sequential numbers from an unordered array of two numbers.

--- public/19-mathFormulas.js
+++ public/19-mathFormulas.js
@@ -6,3 +6,13 @@ console.log(gaussFormula(100)); // 5050
 
 const triangluarNumberSequence = n => (n * (n + 1)) / 2;
 console.log(triangluarNumberSequence(100)); //5050
+
+// Least Common Multiple for a range of sequential numbers from an unordered array of two numbers.
+const leastCommonMultiple = arr => {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  const gcd = (a, b) => a ? gcd(b % a, a) : b;
+  const lcm = (a, b) => a * b / gcd(a, b);
+  return Array.from({length: max - min + 1}, (e, i) => i + min).reduce(lcm);
+}
+console.log(leastCommonMultiple([23, 18]));
